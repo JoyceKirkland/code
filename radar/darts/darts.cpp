bea04501843a5b4162d228e098f73b78987bacb0
joycekirkland <joyce84739879@163.com>
2021-11-11 21:42:52 +0800

    fix:Add identification box

--- radar/darts/darts.cpp
+++ radar/darts/darts.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-02 16:33:40
- * @LastEditTime: 2021-11-08 17:01:06
+ * @LastEditTime: 2021-11-11 21:38:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /radar/Binocular_camera.cpp
@@ -22,7 +22,7 @@ int main ()
 	Mat frame;
 	Mat gray ;
 	Mat hsv;
-	Mat gray_dilate1 ;
+	Mat frame_1 ;
 	Mat gray_dilate2 ;
 	Mat gray_dilate3 ;
 	Mat background,foreground,foreground_BW;
@@ -34,16 +34,19 @@ int main ()
     int fps       = capture.get(CAP_PROP_FPS);          //获取帧率
     int numFrames = capture.get(CAP_PROP_FRAME_COUNT);  //获取整个帧数
 	int num=numFrames;
+	
     printf("vedio's \nwidth = %d\t height = %d\n video's fps = %d\t nums = %d", frameW, frameH, fps, numFrames);  
 	//---------------------------------------------------------------------
-	Mat frame_0,frame_1;//Mat m(3, 5, CV_32FC1, 1);
+	Mat frame_0;//Mat m(3, 5, CV_32FC1, 1);
 	//---------------------------------------------------------------------
+	int count_num=0;
 	while(1)
 	{
-	   double time = (double)getTickCount();
-
+	   count_num=count_num+1;
 	   capture>>frame;
-	   imshow("frame_resize",frame);
+	   
+	   double time = (double)getTickCount();
+	//    imshow("frame_resize",frame);
 	//    cvtColor( frame,hsv, COLOR_RGB2HSV );
 	//    inRange(hsv,Scalar(0,0,249),Scalar(170,120,255),gray);
 	//    imshow("inrange",hsv);
@@ -51,49 +54,94 @@ int main ()
 	   	//    bilateralFilter(gray,mid_filer,10,60,60);
 		GaussianBlur(gray, mid_filer, Size(5, 5), 1, 1);	  
 	   	//    medianBlur(gray,mid_filer,5);     //中值滤波法
-
+		//    cout<<count_num<<endl;
+	   	// frame_0=mid_filer.clone();
+	//    threshold( mid_filer, mid_filer, 100, 255 , 0 );
+	   imshow("mid_filer",mid_filer);
 	   //-----------------------------------------------------------------------------------
 	   //选择前一帧作为背景（读入第一帧时，第一帧作为背景）
-	   if(num==numFrames)
+	//    if(num==numFrames)
+	   if(count_num==1)
 	   {
 		   background=mid_filer.clone();
+		   frame_1=mid_filer.clone();
 		   frame_0=background;
 	   }
 	   else
 	   {
 		    background=frame_0; 
 	   }
-	   //------------------------------------------------------------------------------------
-	   absdiff(mid_filer,background,foreground);//用帧差法求前景
-	//    imshow("foreground",foreground);
-	//    Mat element = getStructuringElement(MORPH_RECT,Size(3,3));
- 
+
 	   
-	   threshold( foreground, foreground_BW, 100, 255 , 0 );//二值化通常设置为50  255
+	   //------------------------------------------------------------------------------------
+	//    imshow("frame",frame_1);
+	//    imshow("background",background);
+	   absdiff(mid_filer,background,foreground);//用帧差法求前景
+	//    absdiff(mid_filer,background,foreground);//用帧差法求前景
+	   Mat element = getStructuringElement(MORPH_RECT,Size(9,9));
+
+	   threshold( foreground, foreground_BW, 120, 255 , 0 );//二值化通常设置为50  255
 	   //threshold(foreground, foreground_BW, 0, 255 ,CV_THRESH_BINARY | CV_THRESH_OTSU) ;  //此处使用大津法  自适应取阈值
 	//    imshow("二值化",foreground_BW);
 	//    bilateralFilter(foreground_BW,gray_dilate2,5,4,4);
-	   	// dilate(mid_filer,gray_dilate1,element);
+	   	dilate(foreground_BW,foreground_BW,element);
 
 	//    absdiff(mid_filer,background,foreground);//用帧差法求前景
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	// Canny(foreground_BW,foreground_BW,150,204,3);//边缘检测
+	findContours(foreground_BW,contours,hierarchy,RETR_EXTERNAL,CHAIN_APPROX_NONE,Point());//寻找并绘制轮廓
+	imshow("foreground_BW",foreground_BW);   
 
+    vector<Moments>mu(contours.size());
+	for(unsigned i=0;i<contours.size();i++)//计算轮廓面积
+    {
+        mu[i]=moments(contours[i],false);
+		// cout<<mu[i].m00<<endl;
+    }
+    for(int i=0;i<contours.size();i++)//最小外接矩形
+    {
+		Rect rect = boundingRect(contours[i]);	//找出轮廓最小外界矩形
+			// cout << "矩形框" << rect.width << endl;
+			if(mu[i].m00<2000&&mu[i].m00>200)
+			{
+				rectangle(frame, rect, Scalar(0, 0, 255), 3);	//在原图像上画出矩形
+			}
+			// if((rect.width > 10) && (rect.width < 400))	//限定矩形框的大小
+				// rectangle(frame, rect, Scalar(0, 0, 255), 3);	//在原图像上画出矩形
+
+	}
+	
+
+	//  for(int i=0;i<contours.size();i++)//最小外接矩形
+    // {
+    //     // drawContours(foreground_BW,contours,i,Scalar(255),1,8,hierarchy);
+    //     // RotatedRect rect=minAreaRect(contours[i]);
+    //     // Point2f P[4];
+    //     // rect.points(P);
+    //     // for(int j=0;j<=3;j++)
+    //     // {
+    //     //     line(frame,P[j],P[(j+1)%4],Scalar(255,255,255),2);
+    //     // }
+	// 	Rect rect = boundingRect(contours[i]);	//找出轮廓最小外界矩形
+	// 		// cout << "矩形框" << rect.width << endl;
+	// 		if((rect.width > 10) && (rect.width < 400))	//限定矩形框的大小
+	// 			rectangle(frame, rect, Scalar(0, 0, 255), 3);	//在原图像上画出矩形
+
+	// }
 	   time = ((double)getTickCount() - time) / getTickFrequency();
        int fps = 1 / time;
-	   imshow("foreground",foreground_BW);
-	//    imshow("gray_dilate2",gray_dilate2);
-	   imshow("mid_filer",mid_filer);
-
+	   
+	   imshow("frame",frame);
+	//    imshow("foreground",foreground);
 
 	   cout<<"fps:"<<fps<<endl;
-	//    imshow("gray_dilate1",gray_dilate1);
-	//    dilate(gray_dilate1,gray_dilate2,element);
-	//    imshow("gray_dilate2",gray_dilate2);
-	//    dilate(gray_dilate2,gray_dilate3,element);
-	//    imshow("gray_dilate3",gray_dilate3);
  
 	   frame_0=mid_filer.clone();
+	   imshow("frame_0",frame_0);
+	//    frame_1=foreground.clone();
 	   num--;
-	   char c = waitKey(1);
+	   char c = waitKey(30);
 	   if( c =='q' ) break;
 	   if (num < 1)
 		   return -1;
