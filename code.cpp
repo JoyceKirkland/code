7d5c1aedd9805e1be76bad173a38c99c01c2aefc
JoyceKirkland <joyce84739879@163.com>
2022-04-17 17:08:02 +0800

    fix:finish serial_bug

--- code.cpp
+++ code.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-18 11:00:18
- * @LastEditTime: 2022-04-17 16:56:56
+ * @LastEditTime: 2022-04-17 17:07:43
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /code/code.cpp
@@ -501,7 +501,7 @@ void uartReadThread(const std::shared_ptr<RoboSerial> &serial,
       out_robo_inf.R_Sentry_HP.store(robo_inf.R_Sentry_HP);
       out_robo_inf.R_Outpost_HP.store(robo_inf.R_Outpost_HP);
       out_robo_inf.R_Base_HP.store(robo_inf.R_Base_HP);
-      
+          //sdfas
       out_robo_inf.B_Hero_HP.store(robo_inf.B_Hero_HP);
       out_robo_inf.B_Engineer_HP.store(robo_inf.B_Engineer_HP);
       out_robo_inf.B_Infantry3_HP.store(robo_inf.B_Infantry3_HP);
